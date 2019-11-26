2911823753d56f15e919812d66862daac66d1d6d
eimarah <trailblazing18@gmail.com>
2019-11-25 22:18:53 -0500

    Test

---
+++ PMTool/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PMTool.Controllers.Api;
+using PMTool.Helper;
+using PMTool.Models;
+
+namespace PMTool.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly DataApi _api = new DataApi();
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
